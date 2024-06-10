5bae126f43dd77f2a8eef1a0fe5fa6c1c65780d7
TheInfamousGrim <george@grimfunky.dev>
2024-06-10 20:31:30 +0100

    ✨ Complete stateful handlers exercise

--- 02-chirpy/main.go
+++ 02-chirpy/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+    fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func (w http.ResponseWriter, req *http.Request) {
+        cfg.fileserverHits++
+        next.ServeHTTP(w, req)
+    })
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+    apiCfg := apiConfig{ fileserverHits: 0 }
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
 
     //* Server ping route
     mux.HandleFunc("/healthz", handleReadiness)
+    mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+    mux.HandleFunc("/reset", apiCfg.handleResetFileHits)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -28,4 +44,19 @@ func handleReadiness(w http.ResponseWriter, req *http.Request) {
     w.Header().Add("Content-Type", "text/plain; charset=utf-8")
     w.WriteHeader(http.StatusOK)
     w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, req *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) handleResetFileHits(w http.ResponseWriter, req *http.Request) {
+    // Reset the file server hits
+    cfg.fileserverHits = 0
+    
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("Hits %d", cfg.fileserverHits)))
 }
